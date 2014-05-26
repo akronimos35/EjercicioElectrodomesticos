493d6bc88e4aab200501f998bba646e6c74f884a
jorgeld <jorgeld1986@gmail.com>
2014-05-26 19:03:53 +0200

    Primer commit de jorge_clientes
    Interfaz gráfica de la pestaña clientes finalizada

---
+++ electrodomesticos_JaviPlus/electrodomesticos_JaviPlus/Database3DataSet.cs
@@ -0,0 +1,10 @@
+﻿namespace electrodomesticos_JaviPlus {
+    
+    
+    public partial class Database3DataSet {
+        partial class ClientesDataTable
+        {
+            D    
+        }
+    }
+}
--- electrodomesticos_JaviPlus/electrodomesticos_JaviPlus/clientes.cs
+++ electrodomesticos_JaviPlus/electrodomesticos_JaviPlus/clientes.cs
@@ -16,5 +16,25 @@ namespace electrodomesticos_JaviPlus
         {
             InitializeComponent();
         }
+
+        private void clientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.clientesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database3DataSet);
+
+        }
+
+        private void clientes_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'database3DataSet.Clientes' Puede moverla o quitarla según sea necesario.
+            this.clientesTableAdapter.Fill(this.database3DataSet.Clientes);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
